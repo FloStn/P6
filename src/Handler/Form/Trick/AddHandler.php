dcab4396b3c1f9e0c25fe7127d65f198d7f8c40e
P6 <flo.stein9578@yahoo.fr>
2018-11-20 12:37:49 +0100

    Calling FileUploader service in the AddHandler class.

--- src/Handler/Form/Trick/AddHandler.php
+++ src/Handler/Form/Trick/AddHandler.php
@@ -3,14 +3,19 @@
 namespace App\Handler\Form\Trick;
 
 use Doctrine\ORM\EntityManagerInterface;
+use App\Events;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
 class AddHandler
 {
     private $entityManager;
+    private $eventDispatcher;
 
-    public function __construct(EntityManagerInterface $entityManagerInterface)
+    public function __construct(EntityManagerInterface $entityManagerInterface, EventDispatcherInterface $eventDispatcher)
     {
         $this->entityManager = $entityManagerInterface;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function handle($trick, $trickForm, $user)
@@ -21,7 +26,10 @@ class AddHandler
             
             $trick->setAuthor($user);
             $trick->setSlug($slug);
-            $this->uploadHandle($trick);
+            
+            $event = new GenericEvent($trick);
+            $this->eventDispatcher->dispatch(Events::IMAGE_FORWARD_UPLOADER, $event);
+            $this->eventDispatcher->dispatch(Events::IMAGE_UPLOADER, $event);
 
             $this->entityManager->persist($trick);
             $this->entityManager->flush();
